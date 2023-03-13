f61a69eccd9380b354120ffca8db95565b08510f
Leonardo Coelho <leo.csd@hotmail.com>
2023-03-13 17:37:24 -0300

    Criando Projeto

---
+++ src/main/java/br/com/jsf/bean/CadastroBean.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.jsf.bean;
+
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+
+
+
+/**
+ *
+ * @author didic
+ */
+@ManagedBean
+public class CadastroBean {
+    
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+    private String endereco;
+    
+    public void gravar(){
+        System.out.println("Gravando");
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+    
+    
+}
